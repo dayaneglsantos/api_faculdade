5e6ed49d91fe6a35e729d87d92913c5bc3f4e646
Dayane Santos <dayaneglsantos@gmail.com>
2026-09-03 09:48:04 -0300

    allow API documentation origin

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,7 +1,8 @@
 const allowedOrigins = [
+  process.env.API_URL,
   process.env.FRONTEND_PROD_URL,
   process.env.FRONTEND_DEV_URL,
-];
+].filter(Boolean);
 
 export const corsConfiguration = {
   origin: (origin, callback) => {
